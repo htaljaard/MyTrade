252a889c3627e2f2316196a372a13af3a4fadda9
Hein Taljaard <hein.taljaard@needcoffee.net>
2025-01-27 13:33:06 +1100

    Initial Commit

---
+++ MyTrade.SharedKernel/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTrade.SharedKernel
+{
+    public class Entity(Guid EntityId)
+    {
+        public Guid Id { get; protected set; } = EntityId;
+
+
+    }
+}
---
+++ MyTrade.SharedKernel/IHasDomainEvents.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTrade.SharedKernel
+{
+    public interface IHasDomainEvents
+    {
+        List<DomainEvent> DomainEvents { get; }
+        void ClearDomainEvents();
+    }
+}
