6186a7eb9c3693770c745b3b2fc14bff56db4fce
Hein Taljaard <hein.taljaard@needcoffee.net>
2025-01-27 14:44:56 +1100

    User Module setup

---
+++ MyTrade.Companies/Domain/Company.cs
@@ -0,0 +1,19 @@
+﻿using MyTrade.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTrade.Companies.Domain
+{
+    internal class Company : Entity
+    {
+        public string ComanyNumber { get; private set; } = string.Empty;
+        public Company(Guid EntityId) : base(EntityId)
+        {
+        }
+
+
+    }
+}
---
+++ MyTrade.Users/Extensions/UserModuleExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTrade.Users.Extensions
+{
+    public static class UserModuleExtensions
+    {
+        public static void AddUserModule(this IServiceCollection services, IConfiguration configuration, List<Assembly> mediatorAssemblies)
+        {
+
+            var connectionString = configuration.GetConnectionString("UsersConnectionString");
+            services.AddDbContext<UsersDBContext>(config => config.UseSqlServer(connectionString));
+        }
+    }
+}
